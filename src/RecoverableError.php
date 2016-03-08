0617a5767307e68eb094b7f8c916dfc8c8516e8a
amekusa <post@amekusa.com>
2016-03-08 17:40:35 +0900

    Add a method: trigger()

--- src/RecoverableError.php
+++ src/RecoverableError.php
@@ -1,4 +1,25 @@
 <?php namespace amekusa\philes; main::required;
 
 class RecoverableError extends \ErrorException {
+
+	/**
+	 * Explicitly triggers a PHP error
+	 * @param boolean $ShowsStackTrace *(optional)* Whether to include the stack trace into the error message
+	 */
+	public function trigger($ShowsStackTrace = false) {
+		$msg = $this->getMessage();
+		if ($ShowsStackTrace) $msg .= "\nStack Trace:\n{$this->getTraceAsString()}";
+		$code = E_USER_WARNING;
+		switch ($this->getCode()) {
+			case E_NOTICE:
+			case E_USER_NOTICE:
+				$code = E_USER_NOTICE;
+				break;
+			case E_DEPRECATED:
+			case E_USER_DEPRECATED:
+				$code = E_USER_DEPRECATED;
+				break;
+		}
+		trigger_error($msg, $code);
+	}
 }
