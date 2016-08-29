86cc85cd75b120e9339727124361e5b3c53ac912
amekusa <post@amekusa.com>
2016-08-29 15:25:25 +0900

    Update 2016-08-29T06:25:20.321Z

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","amekusa\\phio\\Directory"],["c","amekusa\\phio\\ErrorException"],["c","amekusa\\phio\\File"],["c","amekusa\\phio\\FilePool"],["c","amekusa\\phio\\Filter"],["c","amekusa\\phio\\IOException"],["c","amekusa\\phio\\Perms"],["c","amekusa\\phio\\RegexFilter"],["c","amekusa\\phio\\RegFile"],["c","Directory"],["c","ErrorException"],["c","Exception"],["c","IteratorAggregate"],["c","RuntimeException"],["c","Traversable"]];
